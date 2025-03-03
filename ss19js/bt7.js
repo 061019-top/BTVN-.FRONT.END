4b81c10e692b51b2368bb1bbdbbbc520d3ab9a0c
061019-top <chudinhminh21@gmail.com>
2025-03-03 16:04:20 +0700

    update

--- ss19js/bt7.js
+++ ss19js/bt7.js
@@ -1,5 +1,5 @@
 let a = Number(prompt("Mời bạn nhập giá trị cuả số 1"));
 let b = Number(prompt("Mời bạn nhập giá trị cuả số 2"));
 let c = Number(prompt("Mời bạn nhập giá trị cuả số 3"));
-let result = Math.max(a, b, c );
-alert(`Số lớn nhất trong ba số bạn vừa nhập là ${result}`);
+let max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+alert(`Số lớn nhất trong 3 số là ${max}`);
